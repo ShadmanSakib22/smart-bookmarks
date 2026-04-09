16892abf076c445848423714a9741fcd065299bc
Shadman Sakib <sadmansakib10000@gmail.com>
2026-04-09 22:29:23 +0600

    chore: clean up and theme setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+// src/app/app.routes.ts
+
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/bookmarks/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent,
+      ),
+  },
+  { path: '**', redirectTo: '' },
+];
