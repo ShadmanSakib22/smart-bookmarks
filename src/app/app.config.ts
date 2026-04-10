ca52f11d15cac9c2240956f1a5ad9bca98aeb3c5
Shadman Sakib <sadmansakib10000@gmail.com>
2026-04-10 22:29:13 +0600

    feat: lucide setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,56 @@
 // src/app/app.config.ts
-
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZonelessChangeDetection,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import {
+  LucideAngularModule,
+  Trash2,
+  SquareArrowOutUpRight,
+  Copy,
+  Plus,
+  Search,
+  LayoutGrid,
+  Code,
+  Cpu,
+  Users,
+  User,
+  CircleCheckBig,
+  Download,
+  Sun,
+  Moon,
+  Bookmark,
+  SearchX,
+  Link,
+} from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZonelessChangeDetection(), provideRouter(routes)],
+  providers: [
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    importProvidersFrom(
+      LucideAngularModule.pick({
+        Trash2,
+        SquareArrowOutUpRight,
+        Copy,
+        Plus,
+        Search,
+        LayoutGrid,
+        Code,
+        Cpu,
+        Users,
+        User,
+        CircleCheckBig,
+        Download,
+        Sun,
+        Moon,
+        Bookmark,
+        SearchX,
+        Link,
+      }),
+    ),
+  ],
 };
